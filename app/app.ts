2afda2fd747e3dfe971a4a66581dc4fc4b41fcae
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-03-26 00:17:34 -0300

    using let and const

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
+    
+    const playerName = 'Joao';
+    logPlayer(playerName);
+    
     // starting a new game
     var messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
+}
