73455c14dc67deda49572ba566e2d2fdf63cce5d
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-03-27 00:27:38 -0300

    adding type parameters and default values

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,30 @@
 function startGame() {
     
-    let playerName = 'Joao';
+    let playerName: string | undefined = getInputValue('playername');
+    postScore(100, playerName);
     logPlayer(playerName);
     
-    // starting a new game
-    var messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-function logPlayer(name: string) : void {
+function logPlayer(name: string = 'MultiMath Player') : void {
     console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementId: string) : string | undefined {
+
+    const inputElement = <HTMLInputElement>document.getElementById(elementId);
+
+    if (inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value;
+    }
+
+}
+
+function postScore(score: number, player: string = 'MultiMath Player') : void {
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${player}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
