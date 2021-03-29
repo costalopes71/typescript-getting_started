497dada5cf1d61bc88af8b8ff4bf9c2a233d4529
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-03-29 00:49:15 -0300

    converting th demo app to use Modules

--- app/game.ts
+++ app/game.ts
@@ -1,11 +1,11 @@
-/// <reference path="utility.ts" />
-/// <reference path="result.ts" />
-/// <reference path="player.ts" />
-/// <reference path="scoreboard.ts" />
+import { getValue } from "./utility";
+import { Result } from "./result";
+import { Player } from "./player";
+import { Scoreboard as ResultPanel } from "./scoreboard";
 
-class Game {
+export class Game {
 
-    private scoreboard: Scoreboard = new Scoreboard();
+    private scoreboard: ResultPanel = new ResultPanel();
 
     constructor(public player: Player, public problemCount: number, public factor: number) { }
 
@@ -36,7 +36,7 @@ class Game {
         let score: number = 0;
 
         for (let index = 1; index <= this.problemCount; index++) {
-            const answer = Number(Utility.getInputValue('answer' + index));
+            const answer = Number(getValue('answer' + index));
 
             if (index * this.factor === answer) {
                 score++;
